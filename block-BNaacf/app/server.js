c35dfd39bf61467d4133a1505e449cea6624ecb2
sonu <sonurocckkerr@gmail.com>
2020-12-02 14:33:27 +0530

    completed block 6 of schema

---
+++ block-BNaacf/app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/sample",
+    { useNewUrlParser: true, useUnifiedTopology: true}
+    ,(err)=> {
+        console.log(err ? err : "connected to database");
+    }
+);
+
+const app = express();
+
+app.listen(3000,()=> {
+    console.log('server is running on port 3000');
+})
